ded68b7488299b03d728c48a9cf84c96ff1c8153
vmath20 <vaibhavrichmishra@gmail.com>
2024-01-19 23:29:33 -0500

    Create ClinicalBERT_Drug.py

---
+++ ClinicalBERT_Drug.py
@@ -0,0 +1,71 @@
+import pandas as pd
+data = pd.read_csv("DrugTokens.csv",encoding="latin1" )
+
+data.head(50)
+
+data =data.fillna(method ="ffill")
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+data["Sentence #"] = LabelEncoder().fit_transform(data["Sentence #"] )
+
+data.rename(columns={"Sentence #":"sentence_id","Word":"words","Tag":"labels"}, inplace =True)
+
+data["labels"] = data["labels"].str.upper()
+
+X= data[["sentence_id","words"]]
+Y =data["labels"]
+
+x_train, x_test, y_train, y_test = train_test_split(X,Y, test_size =0.2)
+
+train_data = pd.DataFrame({"sentence_id":x_train["sentence_id"],"words":x_train["words"],"labels":y_train})
+test_data = pd.DataFrame({"sentence_id":x_test["sentence_id"],"words":x_test["words"],"labels":y_test})
+
+train_data
+
+!pip install simpletransformers
+
+from simpletransformers.ner import NERModel,NERArgs
+
+label = data["labels"].unique().tolist()
+label
+
+args = NERArgs()
+args.num_train_epochs = 1
+args.learning_rate = 1e-4
+args.overwrite_output_dir =True
+args.train_batch_size = 32
+args.eval_batch_size = 32
+
+from transformers import AutoModelForTokenClassification, AutoTokenizer
+from simpletransformers.ner import NERModel
+
+# Load BioBERT model and tokenizer
+model_name = "medicalai/ClinicalBERT"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+ner_model = AutoModelForTokenClassification.from_pretrained(model_name)
+
+# Initialize Simple Transformers NER model
+model = NERModel(
+    model_type='distilbert',
+    model_name=model_name,
+    labels=label,
+    args=args,
+    use_cuda=False,
+)
+
+model.train_model(train_data,eval_data = test_data,acc=accuracy_score)
+
+result, model_outputs, preds_list = model.eval_model(test_data)
+
+result
+
+prediction, model_output = model.predict(["- continue Cosopt BID, Latanoprost qhs OS"])
+
+prediction
+
+prediction, model_output = model.predict(["- continue Cosopt BID, Latanoprost qhs take Refresh"])
+prediction
+
